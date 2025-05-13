df46ae928cc1b85abf3e0915d668f42174579aaa
nicolas <ndonato@sms-sudamerica.com>
2025-05-13 13:28:36 -0300

    Actualizacion post por REST

--- src/main/java/com/example/msA/client/MiddlewareClient.java
+++ src/main/java/com/example/msA/client/MiddlewareClient.java
@@ -1,14 +1,22 @@
 package com.example.msA.client;
 
 import com.example.msA.dto.UserDetailDTO;
-import com.example.msA.entity.UserDetail;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 // FeignClient a Middleware
-@FeignClient(name = "middleware", url = "http://localhost:8081")
-public interface MiddlewareClient {
-    @PostMapping("/sync-to-legacy")
-    void syncToLegacy(@RequestBody UserDetailDTO userDetail);
+//@FeignClient(name = "middleware", url = "http://localhost:8081")
+//public interface MiddlewareClient {
+//    @PostMapping("/sync-to-legacy")
+//    void syncToLegacy(@RequestBody UserDetailDTO userDetail);
+//}
+
+
+@Component
+public class MiddlewareClient {
+    private final RestTemplate rest = new RestTemplate();
+
+    public void syncToLegacy(UserDetailDTO userDetailDTO) {
+        rest.postForEntity("http://localhost:8081/sync-to-legacy", userDetailDTO, Void.class);
+    }
 }
