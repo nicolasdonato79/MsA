f60ffdb8ed7f7a553b42f5cdf5acf0652f227b66
nicolas <ndonato@sms-sudamerica.com>
2025-05-20 11:19:21 -0300

    Problema de la base solucionado. ahora falta el service-eureka

--- src/main/java/com/example/msA/client/MiddlewareClient.java
+++ src/main/java/com/example/msA/client/MiddlewareClient.java
@@ -1,22 +1,24 @@
 package com.example.msA.client;
 
 import com.example.msA.dto.UserDetailDTO;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 // FeignClient a Middleware
-//@FeignClient(name = "middleware", url = "http://localhost:8081")
-//public interface MiddlewareClient {
-//    @PostMapping("/sync-to-legacy")
-//    void syncToLegacy(@RequestBody UserDetailDTO userDetail);
-//}
-
-
-@Component
-public class MiddlewareClient {
-    private final RestTemplate rest = new RestTemplate();
-
-    public void syncToLegacy(UserDetailDTO userDetailDTO) {
-        rest.postForEntity("http://localhost:8081/sync-to-legacy", userDetailDTO, Void.class);
-    }
+@FeignClient(name = "MSMIDDLEWARE")
+public interface MiddlewareClient {
+    @PostMapping("/msmiddleware/users-details/sync-to-legacy")
+    void syncToLegacy(@RequestBody UserDetailDTO userDetail);
 }
+
+
+//@Component
+//public class MiddlewareClient {
+//    private final RestTemplate rest = new RestTemplate();
+//
+//    public void syncToLegacy(UserDetailDTO userDetailDTO) {
+//        rest.postForEntity("http://localhost:8081/msmiddleware/users-details/sync-to-legacy", userDetailDTO, Void.class);
+//    }
+//}
